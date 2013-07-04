712948cf7139548b59a12480c9ee898a6b01359f
Matthew Ripley <mattew.ripley@gmail.com>
2013-07-03 20:25:45 -0600

    change button size

--- src/com/marbol/marbol/AdventureActivity.java
+++ src/com/marbol/marbol/AdventureActivity.java
@@ -11,7 +11,6 @@ import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.CountDownTimer;
